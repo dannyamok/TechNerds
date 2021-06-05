05fd4c84cda5a1e807d528ff82b88a30b7c112fe
Dannyamok <38763100+dannyamok@users.noreply.github.com>
2021-06-05 08:32:27 +0100

    update

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+import Mongoose from 'mongoose'
+
+const connectDB = async () => {
+  try {
+    const conn = await Mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+
+    console.log(`mongoDB Connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.error(`Error: ${error.message}`)
+    process.exit(1)
+  }
+}
+
+export default connectDB
